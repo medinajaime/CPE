58bf6e492d0011fd3f63bebec17bd39d80809b6a
Jaime Medina <14medinajaime@gmail.com>
2025-12-16 15:57:00 +0800

    init

---
+++ p10079.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    long long n;
+    while(cin >>n)
+    {
+        if(n < 0) return 0;
+
+        printf("%lld\n", 1+ (n*(n+1)/2));
+    }
+
+    return 0;
+}
